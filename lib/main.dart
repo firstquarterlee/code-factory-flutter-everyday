77288c882075b1c1c57161a6901c0f07c82eb128
firstquarterlee <firstquarterlee@gmail.com>
2024-06-04 00:29:30 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
+
+/// StatelessWidget 생성
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF335CB0),
+      body: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: 32.0,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'asset/img/logo.png',
+            ),
+            CircularProgressIndicator(
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
