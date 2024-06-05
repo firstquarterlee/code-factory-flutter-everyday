152d14f425ef73451a200cb90de07ed11eabf782
firstquarterlee <firstquarterlee@gmail.com>
2024-06-06 01:11:06 +0900

    StatefulWidget 연습2, setState함수 추가

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -27,48 +27,11 @@ class _HomeScreenState extends State<HomeScreen> {
                 }
 
                 print('색상 변경: color: $color');
+
+                setState((){});
               },
               child: Text(
                   '색상 변경!'
-              ),
-            ),
-            SizedBox(height: 32.0),
-            Container(
-              width: 50.0,
-              height: 50.0,
-              color: color,
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-///StatelessWidget 실습
-class HomeScreen2 extends StatelessWidget {
-  Color color = Colors.blue;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: (){
-                if(color == Colors.blue){
-                  color = Colors.red;
-                }else{
-                  color = Colors.blue;
-                }
-
-                print('색상 변경: color: $color');
-              },
-              child: Text(
-                '색상 변경!'
               ),
             ),
             SizedBox(height: 32.0),
