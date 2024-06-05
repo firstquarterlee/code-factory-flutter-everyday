5c00854e98db85cc0dc4dc165cdd588e42bf69b7
firstquarterlee <firstquarterlee@gmail.com>
2024-06-06 00:34:36 +0900

    onPressed 연습

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:test_proj/screen/home_screen.dart';
 
 void main() {
+
+  ///플러터 프레임워크가 실행할 준비가 될때까지 기다린다
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(
     MaterialApp(
       home: HomeScreen(),
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
+final homeUrl = Uri.parse('https://blog.codefactory.ai');
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  WebViewController controller = WebViewController()
+  ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..loadRequest(homeUrl);
+
+  HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +17,19 @@ class HomeScreen extends StatelessWidget {
         title: Text('앱바'),
         centerTitle: true,
         backgroundColor: Colors.orange,
+        actions: [
+          IconButton( ///onPressed는 콜백함수다.
+            onPressed: (){
+              controller.loadRequest(homeUrl); ///여기로 이동
+            },
+            icon: Icon(
+              Icons.home
+            ),
+          ),
+        ],
+      ),
+      body: WebViewWidget(
+        controller: controller,
       ),
     );
   }
